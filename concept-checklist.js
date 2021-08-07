b6b8b5aaae91a38f9f79e3fdea8373f49fe8e4b8
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2021-08-06 23:24:03 -0500

    Add files via upload

---
+++ concept-checklist.js
@@ -0,0 +1,31 @@
+// 1. write 3 variables (number, string, boolean)
+
+
+
+// 2. 2 variables using (let, const)
+
+
+// 3. Simple Math Operations() +, -, *, /, %)
+
+
+// 4. comparison ()
+
+// 5. two conditions. case-1: fulfill both conditions. case-2: fulfill at least one condition
+
+// 6. if-else
+
+
+//7. while loop to display 7 to 19 all numbers. only display odd numbers including 7 to 19
+
+// 8. declare an array. number of elements. update or change 4th position element. add or remove elements. check whether a specific value exists in the array
+
+
+// 9. use any for loop to display every elements of an array
+
+
+// 10. you have an array of numbers. display only the numbers bigger than 80
+
+// 11. write a function that takes three numbers and returns the multiplication of the three numbers
+
+// 12. declare an object and change any property of that object.
+
---
+++ problem-solving-checklist.js
@@ -0,0 +1,19 @@
+// 1. conversation
+feetToInch
+
+// 2. conversion
+centimeterToMeter
+
+
+// 3. calculation
+pageRequirements
+
+// book1 has 100 pages
+// book2 has 200 pages
+// book3 has 300 pages
+
+//4. friends
+bestFriend
+
+//5. will stop the loop if the array has any negative number and returns all the positive number before the negative number
+onlyPositive
